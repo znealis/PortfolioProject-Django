ed671e591e676e5df4d6f70e6bead7ca4ca87544
Zach <z.nealis@gmail.com>
2022-12-16 11:32:43 -0500

    Test

--- Portfolio/models.py
+++ Portfolio/models.py
@@ -4,4 +4,6 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    image = models.FilePathField(path='Portfolio/static/img/')
+    def __str__(self):
+        return self.title
