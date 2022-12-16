35512bce9444dba22351465b902961e706566a2b
Zach <z.nealis@gmail.com>
2022-12-16 14:02:23 -0500

    Changed CSS

--- Portfolio/models.py
+++ Portfolio/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path='Portfolio/static/img/')
+    technology = models.TextField()
+    image = models.FileField(upload_to='img/')
     def __str__(self):
         return self.title
