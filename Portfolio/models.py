560b42129e7e66b8da7e4ad019754f12e77406dd
Zach <z.nealis@gmail.com>
2022-12-19 09:54:55 -0500

    added github links

--- Portfolio/models.py
+++ Portfolio/models.py
@@ -4,6 +4,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.TextField()
+    githubLink = models.URLField(max_length=200)
     image = models.FileField(upload_to='img/')
     def __str__(self):
         return self.title
